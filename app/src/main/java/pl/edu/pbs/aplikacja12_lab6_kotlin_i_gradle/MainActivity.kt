c3317a2adb02f6673cb71334fa3cccc42bd74316
piomuc000 <piomuc000@o365.student.pbs.edu.pl>
2023-04-30 14:41:24 +0200

    kod z obrazków

--- app/src/main/java/pl/edu/pbs/aplikacja12_lab6_kotlin_i_gradle/MainActivity.kt
+++ app/src/main/java/pl/edu/pbs/aplikacja12_lab6_kotlin_i_gradle/MainActivity.kt
@@ -1,15 +1,30 @@
 package pl.edu.pbs.aplikacja12_lab6_kotlin_i_gradle
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.edu.pbs.aplikacja12_lab6_kotlin_i_gradle.ui.theme.Aplikacja12_Lab6_Kotlin_i_GradleTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,12 +37,46 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
     }
 }
+
+@Composable
+fun MainContent(movielist:List<String> =listOf("Avatar","3000","PBS","Idą Święta","03.12.2022","Kot","pies","smacznego")) {
+    Column(modifier = Modifier.padding(12.dp)) {
+        LazyColumn{
+            items(items = movielist) {
+                MovieRow(movie = it) { movie -> Log.d("TAG", "MainContent:$movie") }
+            }
+        }
+    }
+}
+
+@Composable
+fun MovieRow(movie: String, onItemClick:(String) -> Unit = {}) {
+    Card(
+        modifier = Modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .height(130.dp)
+            .clickable {
+                onItemClick(movie)
+            },
+        shape = RoundedCornerShape(corner = CornerSize(26.dp)),
+        elevation = 16.dp
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Start) {
+            Surface(modifier = Modifier.padding(12.dp).size(100.dp), shape = RectangleShape, elevation = 4.dp) {
+                Icon(imageVector = Icons.Default.AccountBox, contentDescription = "Movie image")
+            }
+            Text(text = movie)
+        }
+    }
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
